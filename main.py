70141e273ce1846a52c5aa8e3ab8ec75cacaf0d5
dniero95 <darioniero@icloud.com>
2023-01-09 02:37:10 +0100

    add endpoint to see all the movie

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ print(db)
 
 app = FastAPI()
 
+
+
+# Return all the movies in the database
+@app.get('/api/movies')
+async def fetch_all_movies():
+    return db.to_dict('index')
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")

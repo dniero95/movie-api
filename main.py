8d1d12c6915a2074f4998d1f1f08dbf66149f2e9
dniero95 <darioniero@icloud.com>
2023-01-09 02:51:38 +0100

    add endpoint to fetch all movie; add endpoint to fetch a movie by id

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ print(db)
 
 app = FastAPI()
 
-
+# add hello message
+@app.get('/')
+def hello():
+    return {"message": "Welcome in movie-api"}
 
 # Return all the movies in the database
 @app.get('/api/movies')
 async def fetch_all_movies():
     return db.to_dict('index')
+
+# get a movie by id
+
+@app.get('/api/movie/{id}')
+async def get_movie_by_id(id:int):
+    return db.query('MovieID == @id').to_dict('index')
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")

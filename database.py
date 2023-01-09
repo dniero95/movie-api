1eb1d7a592d5dc1ccfcfbd42433275d93bc17ac3
dniero95 <darioniero@icloud.com>
2023-01-09 19:49:37 +0100

    add code to fetch movies from local db

--- database.py
+++ database.py
@@ -10,8 +10,18 @@ movies_collection = db["movies"]
 
 # code from:
 # https://www.geeksforgeeks.org/read-json-file-using-python/
+
+'''Comment this code since the movies are already in the db
+
 with open('./db.json') as file:
     movies_list = json.load(file)
     
 
 x = movies_collection.insert_many(movies_list)
+'''
+
+
+
+for x in movies_collection.find({},{ "_id": 0,"MovieID":1, "MovieTitle": 1, "MovieGenre": 1 }):
+  print(x)
+

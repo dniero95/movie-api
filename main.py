3d46ecc5a7fd8b02d68eb26a833831281a22750e
dniero95 <darioniero@icloud.com>
2023-01-09 02:59:56 +0100

    add delete endpoint

--- main.py
+++ main.py
@@ -25,5 +25,10 @@ async def fetch_all_movies():
 async def get_movie_by_id(id:int):
     return db.query('MovieID == @id').to_dict('index')
 
+@app.delete('/api/delete/movie/{id}')
+async def delete_movie_by_id(id:int):
+    db.drop(db[db.MovieID == id].index, inplace=True)
+    
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")

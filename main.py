158ce65e07ac8866f139acd889ec2628fcfa8cfe
dniero95 <darioniero@icloud.com>
2023-01-09 02:29:03 +0100

    add fake db

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 from fastapi import FastAPI
 import uvicorn
+import pandas as pd
 
+db = pd.read_json('db.json')
+
+print(db)
+
+app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
